9d957f17a3204a3165f01e1f29753c306578acdd
gabriel mascheroni <gabmascheroni@gmail.com>
2019-05-05 20:02:45 -0300

    implementação tabuleiro

---
+++ Source/interfaces/tabuleiro.h
@@ -0,0 +1,120 @@
+#pragma once
+/***************************************************************************
+*
+*	$MCD M�dulo de defini��o:	Módulo Tabuleiro
+*	
+*	Arquivo gerado:			TABULEIRO.h
+*	Letras identificadoras:	TBL
+*
+*	Projeto:	Disciplinas INF 1628 / 1301
+*	Gestor:		DI/PUC-Rio
+*
+*	Autores:	rdms - Rafael Damazio Monteiro da Silva
+*               gmc - Gabriel Garcia Mascheroni Costa
+*
+*
+*	$HA Hist�rico e evolu��o:
+*		Vers�o  Autor	Data		Observa��es
+*		1.00	rdms	01/05/2019	Inicio do desenvolvimento
+*
+*	$ED Descri��o do m�dulo:
+*		Este m�dulo implementa e gerencia o tabuleiro do jogo de gamão
+*
+***************************************************************************/
+
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: TBL Condi��es de retorno
+*
+*
+***********************************************************************/
+
+   typedef enum {
+
+      TBL_notEnoughMemory = -1,
+		   /* Faltou memoria */
+      TBL_ok = 0,
+         /* Executou corretamente */
+      TBL_erro = 1,
+         /* Ocorreu erro na execução */
+      TBL_erroCriarPeca = 2,
+         /* Ocorreu erro na criação da peça */
+      TBL_erroGetCasas = 3,
+         /* Ocorreu ao buscar casas */
+      TBL_erroMovimentarPeca = 4
+         /* Ocorreu erro na movimentação da peça */
+
+   } TBL_CondRet ;
+
+
+/******* TIPOS E FUN��ES EXPORTADOS DO M�DULO *****************************/
+
+typedef struct tabuleiro* TabuleiroHead;
+
+
+/***********************************************************************
+*
+*	$FC Fun��o:	TBL Cria tabuleiro
+*
+*	$ED Descri��o da fun��o:
+*		Esta fun��o cria um tabuleiro.
+*
+*
+***********************************************************************/
+
+
+   TBL_CondRet TBL_CriarTabuleiro(TabuleiroHead* tabuleiro, void   ( * ExcluirValor ) ( void * pDado ) ) ;
+
+
+/***********************************************************************
+*
+*	$FC Fun��o:	TBL Destruir Tabuleiro
+*
+*	$ED Descri��o da fun��o:
+*		Esta fun��o destrói um tabuleiro.
+*
+*
+***********************************************************************/
+
+   TBL_CondRet TBL_DestruirTabuleiro(TabuleiroHead pTab ) ;
+
+
+/***********************************************************************
+*
+*	$FC Fun��o:	TBL Obter casas
+*
+*	$ED Descri��o da fun��o:
+*		Esta fun��o busca todas as casas do tabuleiro e as peças contidas nelas.
+*
+*	$EP Par�metros:
+*		$P	tabuleiro	 -	tabuleiro de onde serão buscadas as casas
+*
+*		$P	casas	 -	Referencia de retorno das casas do tabuleiro
+*
+***********************************************************************/
+
+   TBL_CondRet TBL_ObterCasas(LIS_tppLista *casas, TabuleiroHead tabuleiro);
+
+
+/***********************************************************************
+*
+*	$FC Fun��o:	TBL Destruir Tabuleiro
+*
+*	$ED Descri��o da fun��o:
+*		Esta fun��o destrói um tabuleiro.
+*
+*	$EP Par�metros:
+*		$P	tabuleiro	 -	tabuleiro onde serão movidas as peças
+*
+*		$P	casaInicio -	casa de onde a peça movida sairá.
+*
+*		$P	casaFim -	casa para onde a peça se moverá.
+*
+***********************************************************************/
+
+   TBL_CondRet TBL_MoverPeca(TabuleiroHead tabuleiro, int casaInicio, int casaFim ) ;
+
+
+
+
---
+++ Source/tabuleiro/tabuleiro.c
@@ -0,0 +1,182 @@
+/********************************************************************************************
+*	$MCI Módulo de implementação:	Tabuleiro para Gamão
+*
+*	Arquivo gerado:			TABULEIRO.c
+*	Letras identificadoras:	TBL
+*
+*	Projeto:	Disciplinas INF 1628 / 1301
+*	Gestor:		DI/PUC-Rio
+*
+*	Autores:	rdms - Rafael Damazio Monteiro da Silva
+*               gmc - Gabriel Garcia Mascheroni Costa
+*
+*	$HA Hist�rico e evolu��o:
+*		Vers�o  Autor	Data		Observa��es
+*		1.00	gmc	01/05/2019	Inicio do desenvolvimento
+*
+*******************************************************************************************/
+
+#include "../interfaces/tabuleiro.h"
+#include "../interfaces/LISTA.H"
+#include "../interfaces/peca.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: TBL Tabuleiro
+*
+*  $ED Descri��o do tipo
+*		Lista de lista com as casas e peças do tabuleiro
+*
+***********************************************************************/
+
+typedef struct tabuleiro {
+
+	LIS_tppLista Casas;
+
+
+} Tabuleiro;
+
+
+/*******************************************************/
+/*****  Código das funções exportadas pelo módulo  *****/
+/*******************************************************/
+
+/***************************************************************************
+*	$FC Fun��o:	TBL Create Board
+*  
+**********************************************************************/
+
+TBL_CondRet TBL_CriarTabuleiro(TabuleiroHead * tabuleiro, void ( * ExcluirValor ) ( void * pDado ) )
+{
+	int i, addBranca = 0,addPreta = 0;
+	LIS_tppLista casasTabuleiro[24]; 
+	PecaHead pecasBrancas[15]; 
+	PecaHead pecasPretas[15]; 
+
+	*tabuleiro = (TabuleiroHead)malloc(sizeof(Tabuleiro));
+	(*tabuleiro)->Casas = LIS_CriarLista(ExcluirValor);
+
+	/* Criação das peças */
+	for(i = 0; i < 15; i++)
+	{
+		pecasBrancas[i] = NULL;
+		if(Pec_CriarPeca(COLOR_White, &pecasBrancas[i]) != PEC_ok)
+			return TBL_erroCriarPeca;
+
+		pecasPretas[i] = NULL;
+		if(Pec_CriarPeca(COLOR_Black, &pecasPretas[i]) != PEC_ok)
+			return TBL_erroCriarPeca;
+	}
+
+	/* Criação das casas*/
+	for(i = 0; i < 24; i++)
+	{
+		casasTabuleiro[i] = NULL;
+		casasTabuleiro[i] = LIS_CriarLista(ExcluirValor);
+	}
+
+	// Preencher as casas com as posições iniciais das peças
+	LIS_InserirElementoApos(casasTabuleiro[0], pecasBrancas[addBranca]); addBranca++;
+	LIS_InserirElementoApos(casasTabuleiro[0], pecasBrancas[addBranca]); addBranca++;
+	LIS_InserirElementoApos(casasTabuleiro[23], pecasPretas[addPreta]); addPreta++;
+	LIS_InserirElementoApos(casasTabuleiro[23], pecasPretas[addPreta]); addPreta++;
+
+	for(i = 0; i < 5; i++)
+	{
+		LIS_InserirElementoApos(casasTabuleiro[11], pecasBrancas[addBranca]); addBranca++;
+		LIS_InserirElementoApos(casasTabuleiro[18], pecasBrancas[addBranca]); addBranca++;
+
+		LIS_InserirElementoApos(casasTabuleiro[5], pecasPretas[addPreta]); addPreta++;
+		LIS_InserirElementoApos(casasTabuleiro[12], pecasPretas[addPreta]); addPreta++;
+	}
+
+	for(i = 0; i < 3; i++)
+	{
+		LIS_InserirElementoApos(casasTabuleiro[16], pecasBrancas[addBranca]); addBranca++;
+		LIS_InserirElementoApos(casasTabuleiro[7], pecasPretas[addPreta]); addPreta++;		
+	}
+
+	// Criar lista de casas no tabuleiro
+	for(i = 0; i < 24; i++)
+		LIS_InserirElementoApos((*tabuleiro)->Casas, casasTabuleiro[i]);
+
+	return TBL_ok;
+}
+
+/***************************************************************************
+*	$FC Fun��o:	TBL Destruir Tabuleiro
+*  
+**********************************************************************/
+
+TBL_CondRet TBL_DestruirTabuleiro (TabuleiroHead tabuleiro)
+{
+	LIS_DestruirLista(tabuleiro->Casas);
+	free(tabuleiro);
+	tabuleiro = NULL;
+	return TBL_ok;
+}
+
+/***************************************************************************
+*	$FC Fun��o:	TBL Obter Casas
+*  
+**********************************************************************/
+
+TBL_CondRet TBL_ObterCasas(LIS_tppLista *casas, TabuleiroHead tabuleiro)
+{
+	*casas = tabuleiro->Casas;
+	return TBL_ok;
+}
+
+/***************************************************************************
+*	$FC Fun��o:	TBL Mover Peca
+*  
+**********************************************************************/
+
+TBL_CondRet TBL_MoverPeca(TabuleiroHead tabuleiro, int casaInicio, int casaFim ) 
+{
+	LIS_tppLista listaAux;
+	PecaHead pecaAux1, pecaAux2;
+	PEC_color color;
+
+	// Pegar lista de peças na casa inicial
+	IrInicioLista(tabuleiro->Casas);
+	LIS_AvancarElementoCorrente(tabuleiro->Casas, casaInicio);
+	listaAux = (LIS_tppLista)LIS_ObterValor(tabuleiro->Casas);
+
+	// pegar uma peça da casa inicial
+	pecaAux1 = (PecaHead)LIS_ObterValor(listaAux);
+
+	// se a casa não estiver vazia, buscar cor da peça, senão dar throw
+	if(pecaAux1 != NULL)
+	{
+		Pec_ObterCor(&color, pecaAux1);
+	}
+	else 
+	{
+		return TBL_erroMovimentarPeca;
+	}
+
+	// remover peça da casa inicial
+	if(LIS_ExcluirElemento(listaAux) != LIS_CondRetOK)
+		return TBL_erroMovimentarPeca;
+
+	// Pegar lista de peças na casa final
+	IrInicioLista(tabuleiro->Casas);
+	LIS_AvancarElementoCorrente(tabuleiro->Casas, casaFim);
+	listaAux = (LIS_tppLista)LIS_ObterValor(tabuleiro->Casas);
+
+	// Criar nova peça
+	if(Pec_CriarPeca(color, &pecaAux2) != PEC_ok)
+		return TBL_erroCriarPeca;
+
+	// Adicionar nova peça na casa
+	if(LIS_InserirElementoApos(listaAux, pecaAux2) != LIS_CondRetOK)
+		return TBL_erroMovimentarPeca;
+
+	return TBL_ok;
+}
+
+
