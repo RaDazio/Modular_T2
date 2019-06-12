1897313ffb0c4424892daedf6773d7438b472218
Rafael Damazio Monteiro da Silva <rafaeldamazio@tecgraf.puc-rio.br>
2019-06-12 12:57:19 -0300

    mais avanço

--- Source/interfaces/user_interface.h
+++ Source/interfaces/user_interface.h
@@ -2,12 +2,14 @@
 
 #include "peca.h"
 
-void RenderizarTabuleiro();
+void RenderizarTabuleiro(int pontuacao_preta, int pontuacao_branca);
 
 void LimparRender();
 
-void RenderizarJogadaAtual(PEC_color jogadorAtual, int dices[4], int qtd_dice_valid, int vector[24], int qtd_casas, int pode_dobrar);
+void RenderizarJogadaAtual(PEC_color jogadorAtual, int dices[4], int qtd_dice_valid, int vector[24], int qtd_casas);
 
 void Menu_Inicial(int* resposta);
 
-void Menu_Sair(int *resposta);;
+void Menu_Sair(int *resposta);
+
+void RenderizarMenuDePontos(int *response, PEC_color jogador_da_vez);
